e873da196242fd5d95d0942e7653b15535db36ff
devanshitw <56011944+devanshitw@users.noreply.github.com>
2022-11-14 02:23:58 +0530

    Add files via upload

---
+++ lobby.js
@@ -0,0 +1,13 @@
+let form = document.getElementById('lobby__form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    sessionStorage.setItem('display_name', e.target.name.value)
+
+    let inviteCode = e.target.room.value
+    if(!inviteCode){
+        inviteCode = String(Math.floor(Math.random() * 10000))
+    }
+    window.location = `room.html?room=${inviteCode}`
+})
